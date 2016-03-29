197fc102020cbd4bc77067fda79d26e8c846b597
pirklk <pirklk@gmail.com>
2016-03-29 11:02:18 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -11,7 +11,7 @@ class skeleton {
     owner => 'root',
     group => 'root',
     mode => '0644',
-    source => "puppet:///modules/skeleton/bashrc",
+    source => "puppet:///modules/skeleton/.bashrc",
   }  
   
 }
