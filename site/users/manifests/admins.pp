73cf61da44dfcc046d47bb34ba0a5fc395f448ab
pirklk <pirklk@gmail.com>
2016-03-30 13:26:32 -0700

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,1 +1,17 @@
-include users::admins
+class users::admins {
+
+  users::managed_user {'joe':}
+  
+  users::managed_user {'alice':
+    group => 'staff',
+  }
+  
+  users::managed_user {'aaron':
+    group => 'staff',
+  }
+  
+  group {'staff':
+    ensure => present,
+  }
+  
+}
