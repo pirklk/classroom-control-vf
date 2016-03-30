068fac9094802c82a61a603e53d08f9281296f7a
pirklk <pirklk@gmail.com>
2016-03-30 09:18:36 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,39 +6,30 @@ class nginx {
 		group => 'root',
 		mode => '0664',
 	}
-
 	package { 'nginx':
 		ensure => present,
 	}
-	
-	file { ["$(varwww)", '/etc/nginx/conf.d']:
+	file { [$varwww, '/etc/nginx/conf.d']:
 		ensure => directory,
 	}	
-	
-	file { "$(varwww)/index.html":
+	file { "${varwww}/index.html":
 		ensure => file,
 		source => 'puppet:///modules/nginx/index.html',
 	}
-	
-	file { "$(etcnginx)/nginx.conf":
+	file { "${etcnginx}/nginx.conf":
 		ensure => file,
 		source => 'puppet:///modules/nginx/nginx.conf',
 		require => Package['nginx'],
 		notify => Service['nginx'],
 	}
-	
-	
-	file { "$(etcnginx)/conf.d/default.conf":
+	file { "${etcnginx}/conf.d/default.conf":
 		ensure => file,
 		source => 'puppet:///modules/nginx/default.conf',
 		require => Package['nginx'],
 		notify => Service['nginx'],
 	}
-	
 	service { 'nginx':
 		ensure => running,
 		enable => true,
 	}
-	
-
 }
