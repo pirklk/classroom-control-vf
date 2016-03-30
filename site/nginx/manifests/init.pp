a07e22764a5163bf822e3c450ea7b793f5f43dae
pirklk <pirklk@gmail.com>
2016-03-30 09:21:38 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,35 +1,35 @@
 class nginx {
-	$varwww = '/var/www'
-	$etcnginx = '/etc/nginx'
-	File {
-		owner => 'root',
-		group => 'root',
-		mode => '0664',
-	}
-	package { 'nginx':
-		ensure => present,
-	}
-	file { [$varwww, '/etc/nginx/conf.d']:
-		ensure => directory,
-	}	
-	file { "${varwww}/index.html":
-		ensure => file,
-		source => 'puppet:///modules/nginx/index.html',
-	}
-	file { "${etcnginx}/nginx.conf":
-		ensure => file,
-		source => 'puppet:///modules/nginx/nginx.conf',
-		require => Package['nginx'],
-		notify => Service['nginx'],
-	}
-	file { "${etcnginx}/conf.d/default.conf":
-		ensure => file,
-		source => 'puppet:///modules/nginx/default.conf',
-		require => Package['nginx'],
-		notify => Service['nginx'],
-	}
-	service { 'nginx':
-		ensure => running,
-		enable => true,
-	}
+    $varwww     = '/var/www'
+    $etcnginx   = '/etc/nginx'
+    File {
+    	owner   => 'root',
+    	group   => 'root',
+    	mode    => '0664',
+    }
+    package { 'nginx':
+    	ensure  => present,
+    }
+    file { [$varwww, '/etc/nginx/conf.d']:
+    	ensure  => directory,
+    }	
+    file { "${varwww}/index.html":
+    	ensure  => file,
+    	source  => 'puppet:///modules/nginx/index.html',
+    }
+    file { "${etcnginx}/nginx.conf":
+    	ensure  => file,
+    	source  => 'puppet:///modules/nginx/nginx.conf',
+    	require => Package['nginx'],
+    	notify  => Service['nginx'],
+    }
+    file { "${etcnginx}/conf.d/default.conf":
+    	ensure  => file,
+    	source  => 'puppet:///modules/nginx/default.conf',
+    	require => Package['nginx'],
+    	notify  => Service['nginx'],
+    }
+    service { 'nginx':
+    	ensure => running,
+    	enable => true,
+    }
 }
