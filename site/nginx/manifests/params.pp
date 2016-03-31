be63de0f6d66176503cbf67019705133d4031112
pirklk <pirklk@gmail.com>
2016-03-31 13:56:20 -0700

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,29 +1,30 @@
 class nginx::params {
   case $::osfamily {
-    'redhat','debian' : {
+    'redhat', 'debian': {
       $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
+      $owner   = 'root'
+      $group   = 'root'
       $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
+      $logdir  = '/var/log/nginx'
+      $docroot = '/var/www'
     }
-    'windows' : {
+    'windows': {
       $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      $owner   = 'Administrator'
+      $group   = 'Administrators'
       $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs'
+      $logdir  = 'C:/ProgramData/nginx/logs'
+      $docroot = 'C:/ProgramData/nginx/html'
     }
-    default : {
-      fail("Module ${module_name} is not supported on ${::osfamily}")
-    }
+    default: { fail("OS Family ${::osfamily} is not supported with this nginx module") }
   }
+
+  ## Because Redhat and Debian are different here, we can't specify the user of
+  ## of the nginx service in the big case statement above. Thus we have the
+  ## selector here...
   $user = $::osfamily ? {
-    'redhat' => 'nginx',
-    'debian' => 'www-data',
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
     'windows' => 'nobody',
   }
-}
-
+ }
