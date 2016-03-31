100ffdf32caedfe2c8da84b9edbb93867d557156
pirklk <pirklk@gmail.com>
2016-03-31 13:52:48 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,48 @@
 class nginx (
-$package = $nginx::params::package,
-$owner = $nginx::params::owner,
-$group = $nginx::params::group,
-$docroot = $nginx::params::docroot,
-$confdir = $nginx::params::confdir,
-$logdir = $nginx::params::logdir,
-$user = $nginx::params::user,
+  $package = $nginx::params::package,
+  $owner   = $nginx::params::owner,
+  $group   = $nginx::params::group,
+  $confdir = $nginx::params::confdir,
+  $logdir  = $nginx::params::logdir,
+  $docroot = $nginx::params::docroot,
+  $root    = $nginx::params::root,
 ) inherits nginx::params {
-File {
-owner => $owner,
-group => $group,
-mode => '0664',
-}
-package { $package:
-ensure => present,
-}
-file { [ $docroot, "${confdir}/conf.d" ]:
-ensure => directory,
-}
-file { "${docroot}/index.html":
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { "${confdir}/nginx.conf":
-ensure => file,
-content => template('nginx/nginx.conf.erb'),
-notify => Service['nginx'],
-}
-file { "${confdir}/conf.d/default.conf":
-ensure => file,
-content => template('nginx/default.conf.erb'),
-notify => Service['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-}
+  File {
+    owner => $owner,
+    group => $group,
+    mode  => '0644',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  package { 'nginx':
+    ensure => installed,
+    name   => $package,
+  }
+  
+  file { "${confdir}/nginx.conf":
+    ensure => file,
+    #source => 'puppet:///modules/nginx/nginx.conf',
+    content => template('nginx/nginx.conf.erb'),
+  }
+  
+  file { "${confdir}/conf.d/default.conf":
+    ensure => file,
+    #source => 'puppet:///modules/nginx/default.conf',
+    content => template('nginx/default.conf.erb'),
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+  
+  file { $docroot:
+    ensure => directory,
+  }
+  
+  file { "${docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
 }
