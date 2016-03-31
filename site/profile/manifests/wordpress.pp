ba33ff89e9ee2ad53936a1083da1a3c55fea2819
pirklk <pirklk@gmail.com>
2016-03-31 14:19:49 -0700

    Create wordpress.pp

---
+++ site/profile/manifests/wordpress.pp
@@ -0,0 +1,39 @@
+class profile::wordpress {
+	# Mysql Server
+	class { '::mysql::server':
+	  root_password           => 'password',
+	}
+
+	# WordPress Config
+
+	# Apache VHost Config
+	class { 'apache': 
+		default_vhost => false,
+	}
+	include apache::mod::php 
+	apache::vhost { $::fqdn:
+	  port    => '80',
+	  docroot => '/var/www/wordpress',
+	}
+
+	
+
+	# Setup Wordpress
+	class { '::wordpress': 	
+		wp_owner    => 'wordpress',
+		wp_group    => 'wordpress',
+		install_dir => '/var/www/wordpress',
+
+	}
+
+	#Local User for Wordpress
+	user { 'wordpress': 
+		ensure => present,
+	}
+
+	#Local Group for wordpress
+
+	group { 'wordpress': 
+		ensure => present,
+	}
+}
