94addbd00ace406e70f1e5a27b1dbe2d682061a2
pirklk <pirklk@gmail.com>
2016-03-31 15:14:04 -0700

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,42 +1,54 @@
 class profile::wordpress {
-	# Mysql Server
-	class { '::mysql::server':
-	  root_password => 'password',
-	}
 
-	class { '::mysql::bindings':
-		php_enable => true,	
-	}
-
-	# WordPress Config
-
-	# Apache VHost Config
-	class { 'apache': 
-		default_vhost => false,
-	}
-	include apache::mod::php 
-	apache::vhost { 'localhost':
-	  port    => '80',
-	  docroot => '/var/www/wordpress',
-	}
-
-	# Setup Wordpress
-	class { '::wordpress': 	
-		wp_owner    => 'wordpress',
-		wp_group    => 'wordpress',
-		install_dir => '/var/www/wordpress',
-
-	}
-
-	#Local User for Wordpress
-	user { 'wordpress': 
-		ensure => present,
-	}
-
-	#Local Group for wordpress
-
-	group { 'wordpress': 
-		ensure => present,
-	}
+  ##apache
+  class { 'apache':
+    default_vhost => false,
+  }
+  
+  apache::vhost { 'localhost':
+    port    => '80',
+    docroot => '/var/www/wordpress',
+  }
+  
+  include apache::mod::php
+  
+  ##Mysql server
+  class { '::mysql::server':
+    root_password           => 'strongpassword',
+    remove_default_accounts => true,
+  }
+  
+  class { 'mysql::bindings':
+    php_enable => true,
+  }  
+  
+  #wordpress config
+  
+  package {'wget':
+    ensure => present,
+  }
+  
+  #setup wordpress
+  class {'::wordpress':
+    wp_owner => 'wordpress',
+    wp_group => 'wordpress',
+    wp_proxy_host => 'http://proxy-us.intel.com',
+    wp_proxy_port => '911',
+    db_user        => 'wordpress',
+    db_password    => 'strongpassword2',    
+    install_dir => '/var/www/wordpress',
+    require => [
+      User['wordpress'],
+      Group['wordpress'],
+    ]
+  }
+  #local user
+  user { 'wordpress':
+    ensure => present,
+  }
+  
+  #local group
+  group { 'wordpress':
+    ensure => present,
+  }
 }
-
