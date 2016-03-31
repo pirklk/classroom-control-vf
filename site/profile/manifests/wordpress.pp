29ece4968932d128e365ad7d03903b70e7ff882f
pirklk <pirklk@gmail.com>
2016-03-31 15:10:28 -0700

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,46 +1,40 @@
 class profile::wordpress {
-
 	# Mysql Server
 	class { '::mysql::server':
 	  root_password => 'password',
-	  remove_default_accounts => true,
 	}
-	
-	class { '::mysql::bindings' :
-		php_enable => true
+
+	class { '::mysql::bindings':
+		php_enable => true,	
 	}
-	
+
 	# WordPress Config
-	
+
 	# Apache VHost Config
 	class { 'apache': 
 		default_vhost => false,
 	}
-	
-	include apache::mod::php 	
-	apache::vhost { 'localhost' :
+	include apache::mod::php 
+	apache::vhost { 'localhost':
 	  port    => '80',
 	  docroot => '/var/www/wordpress',
 	}
-	
+
 	# Setup Wordpress
 	class { '::wordpress': 	
 		wp_owner    => 'wordpress',
 		wp_group    => 'wordpress',
-#		wp_proxy_host => 'http://proxy-us.intel.com',
-#		wp_proxy_port => '911',
-		db_user        => 'wordpress',
-		db_password    => 'strongpassword2',    		
 		install_dir => '/var/www/wordpress',
-		require => [
-			User['wordpress'],
-			Group['wordpress'],
-		]
+
 	}
 
+	#Local User for Wordpress
 	user { 'wordpress': 
 		ensure => present,
 	}
+
+	#Local Group for wordpress
+
 	group { 'wordpress': 
 		ensure => present,
 	}
